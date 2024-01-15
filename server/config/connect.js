f8977df63202823a87f82648ce581781350981b9
Ayush Gupta <127961367+AyusGup@users.noreply.github.com>
2024-01-16 01:08:23 +0530

    Update connect.js

--- server/config/connect.js
+++ server/config/connect.js
@@ -1,7 +1,7 @@
 const mongoose= require('mongoose')
 
 mongoose
-    .connect("mongodb+srv://ag8350961:taosOMECiJy9yXn@cluster0.cr82fqb.mongodb.net/hostel")
+    .connect("mongodb+srv://ag8350961:taosOMECiPJY9yXn@cluster0.cr82fqb.mongodb.net/hostel")
     .then(()=>{console.log("Db connected")})
     .catch((err)=>{console.log(err)})
 
