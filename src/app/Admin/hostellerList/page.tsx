0e2bfa4e21f134c2f989f244ee7dfce0d66a51f5
Ayush Gupta <127961367+AyusGup@users.noreply.github.com>
2024-06-02 18:04:27 +0530

    Update page.tsx

--- src/app/Admin/hostellerList/page.tsx
+++ src/app/Admin/hostellerList/page.tsx
@@ -42,7 +42,7 @@ export default function Page({ searchParams } : {
                   {data.length && data.map((res:{_id:string; username: string; email: string; tower:string; hostel_room_no:string; descritpion: string; contact: number; userImage: string},idx:number)=>(
                      <tr key={idx} >
                      <th>{idx+1}</th> 
-                     <td><img className="w-10 h-10 rounded-lg border border-gray-300" src={res.userImage} alt="User" /></td>
+                     <td><img className="w-10 h-10 rounded-lg border border-gray-300" src={res.userImage? res.userImage: "/avatar.png"} alt="User" /></td>
                      <td>{res.username}</td> 
                      <td>{res.tower}</td> 
                      <td>{res.hostel_room_no}</td>
@@ -62,7 +62,7 @@ export default function Page({ searchParams } : {
                <div className="card-body">
                <div className="flex-col flex-wrap">
                   <div className="flex flex-wrap">
-                     <div className="mr-0"><img className="w-20 h-20 rounded-xl border border-gray-300" src={res.userImage} alt="User" /></div>
+                     <div className="mr-0"><img className="w-20 h-20 rounded-xl border border-gray-300" src={res.userImage? res.userImage: "/avatar.png"} alt="User" /></div>
                   </div>
                   <div className="flex flex-wrap">
                      <div className="mr-1 font-bold">Name:</div>
